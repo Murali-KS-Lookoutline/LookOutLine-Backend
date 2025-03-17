49b7757d643a8ae20884088ecb7c95ee45409739
Adith <adith776020@gmail.com>
2025-03-17 13:42:24 +0530

    New

--- controllers/productController.js
+++ controllers/productController.js
@@ -25,7 +25,6 @@ const createProduct = async (req, res) => {
   }
 };
 
-// Get All Products (with filtering and pagination)
 const getProducts = async (req, res) => {
   try {
     const {
@@ -58,7 +57,14 @@ const getProducts = async (req, res) => {
     const skip = (page - 1) * limit;
 
     const products = await Product.find(filter).skip(skip).limit(limit);
-    res.status(200).json({ page, limit, products });
+
+    // Convert _id to id
+    const formattedProducts = products.map((product) => ({
+      id: product._id,
+      ...product.toObject(),
+    }));
+
+    res.status(200).json({ page, limit, products: formattedProducts });
   } catch (err) {
     res
       .status(400)
@@ -66,7 +72,6 @@ const getProducts = async (req, res) => {
   }
 };
 
-// Get Product by ID
 const getProductById = async (req, res) => {
   try {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
@@ -77,7 +82,14 @@ const getProductById = async (req, res) => {
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
-    res.status(200).json(product);
+
+    // Convert _id to id
+    const formattedProduct = {
+      id: product._id,
+      ...product.toObject(),
+    };
+
+    res.status(200).json(formattedProduct);
   } catch (err) {
     res
       .status(400)
