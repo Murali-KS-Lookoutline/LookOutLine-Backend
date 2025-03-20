0c96f467702fbbab07624ec33171e17096bbe073
Adith <adith776020@gmail.com>
2025-03-20 15:07:47 +0530

    Get User Data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router
   .route("/:id")
   .delete(protect, authorize("admin"), deleteUser)
-  .put(protect, authorize("admin"), updateUser)
-  .get(protect, authorize("admin"), getUserProfile);
+  .put(protect, updateUser)
+  .get(protect, getUserProfile);
 
 module.exports = router;
